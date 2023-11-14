fd3a12b2b7e21c58145945b211623bf20fa1318c
Ткачик Андрей <68420139+AndreyTkachik@users.noreply.github.com>
2023-11-14 21:23:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,102 @@
-import sys
+import tkinter as tk
+from tkinter import filedialog
 import caesar_cypher as cc
 import vigener_cypher as vic
 import vernam_cypher as vec
 
 
-mode = sys.argv[1]
-filename = sys.argv[2]
-
-if mode == "caesar_encrypt":
-    key = int(sys.argv[3])
-    with open(filename, "r") as file:
+def open_file():
+    file_path = filedialog.askopenfilename()
+    with open(file_path, "r") as file:
         text = file.read()
+    return text, file_path
+
+
+def save_file(text, file_path):
+    with open(file_path, "w") as file:
+        file.write(text)
+
+
+def caesar_encrypt(key):
+    text, file_path = open_file()
     encrypted_text = cc.caesar_encrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(encrypted_text)
-    print("Encryption completed. Encrypted text in", filename)
+    save_file(encrypted_text, file_path)
+    result_label.config(text="Encryption completed. Encrypted text in " + file_path)
 
-elif mode == "caesar_decrypt":
-    key = int(sys.argv[3])
-    with open(filename, "r") as file:
-        text = file.read()
+def caesar_decrypt(key):
+    text, file_path = open_file()
     decrypted_text = cc.caesar_decrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(decrypted_text)
-    print("Decryption completed. Decrypted text in", filename)
+    save_file(decrypted_text, file_path)
+    result_label.config(text="Decryption completed. Decrypted text in " + file_path)
 
-elif mode == "vigenere_encrypt":
-    key = sys.argv[3]
-    with open(filename, "r") as file:
-        text = file.read()
+
+def vigenere_encrypt(key):
+    text, file_path = open_file()
     encrypted_text = vic.vigenere_encrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(encrypted_text)
-    print("Encryption completed. Encrypted text in", filename)
+    save_file(encrypted_text, file_path)
+    result_label.config(text="Encryption completed. Encrypted text in " + file_path)
 
-elif mode == "vigenere_decrypt":
-    key = sys.argv[3]
-    with open(filename, "r") as file:
-        text = file.read()
+
+def vigenere_decrypt(key):
+    text, file_path = open_file()
     decrypted_text = vic.vigenere_decrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(decrypted_text)
-    print("Decryption completed. Decrypted text in", filename)
+    save_file(decrypted_text, file_path)
+    result_label.config(text="Decryption completed. Decrypted text in " + file_path)
 
-elif mode == "vernam_encrypt":
-    key = sys.argv[3]
-    with open(filename, "r") as file:
-        text = file.read()
+
+def vernam_encrypt(key):
+    text, file_path = open_file()
     encrypted_text = vec.vernam_encrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(encrypted_text)
-    print("Encryption completed. Encrypted text in", filename)
+    save_file(encrypted_text, file_path)
+    result_label.config(text="Encryption completed. Encrypted text in " + file_path)
 
-elif mode == "vernam_decrypt":
-    key = sys.argv[3]
-    with open(filename, "r") as file:
-        text = file.read()
+
+def vernam_decrypt(key):
+    text, file_path = open_file()
     decrypted_text = vec.vernam_decrypt(text, key)
-    with open(filename, "w") as file:
-        file.write(decrypted_text)
-    print("Decryption completed. Decrypted text in", filename)
+    save_file(decrypted_text, file_path)
+    result_label.config(text="Decryption completed. Decrypted text in " + file_path)
 
-elif mode == "caesar_brute_force":
-    with open(filename, "r") as file:
-        text = file.read()
-    decrypted_texts = cc.caesar_brute_force(text)
-    for key, decrypted_text in enumerate(decrypted_texts):
-        print(f"Key: {key}, Decrypted text:")
-        print(decrypted_text)
 
-elif mode == "caesar_frequency_analysis_decrypt":
-    with open(filename, "r") as file:
-        text = file.read()
+def caesar_frequency_analysis_decrypt():
+    text, file_path = open_file()
     keys = cc.caesar_frequency_analysis_decrypt(text)
+    decrypted_text = ""
     for key in keys:
-        print(f"Key: {key} Decrypted text:")
-        print(cc.caesar_decrypt(text, key))
+        decrypted_text += "Key: " + str(key) + " Decrypted text:\n"
+        decrypted_text += cc.caesar_decrypt(text, key)
+    save_file(decrypted_text, file_path)
+    result_label.config(text="Frequency analysis completed. Decrypted text in " + file_path)
 
-else:
-    print("Invalid mode specified.")
+
+root = tk.Tk()
+root.title("Text Encryption/Decryption")
+
+key_entry = tk.Entry(root)
+key_entry.pack()
+
+encrypt_button = tk.Button(root, text="Caesar Encrypt", command=lambda: caesar_encrypt(int(key_entry.get())))
+encrypt_button.pack()
+
+encrypt_button = tk.Button(root, text="Vigenere Encrypt", command=lambda: vigenere_encrypt(key_entry.get()))
+encrypt_button.pack()
+
+encrypt_button = tk.Button(root, text="Vernam Encrypt", command=lambda: vernam_encrypt(key_entry.get()))
+encrypt_button.pack()
+
+decrypt_button = tk.Button(root, text="Caesar Decrypt", command=lambda: caesar_decrypt(int(key_entry.get())))
+decrypt_button.pack()
+
+decrypt_button = tk.Button(root, text="Vigenere Decrypt", command=lambda: vigenere_decrypt(key_entry.get()))
+decrypt_button.pack()
+
+decrypt_button = tk.Button(root, text="Vernam Decrypt", command=lambda: vernam_decrypt(key_entry.get()))
+decrypt_button.pack()
+
+decrypt_button = tk.Button(root, text="Frequency analysis Caesar Decrypt", command=lambda: caesar_frequency_analysis_decrypt())
+decrypt_button.pack()
+
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+root.mainloop()
